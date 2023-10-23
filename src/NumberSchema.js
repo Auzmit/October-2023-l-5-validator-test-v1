74a30b7f27672014985b00c0a6cdc7843bec9daf
auzmit <atrudkov@mail.ru>
2023-10-23 15:24:34 +0300

    all solutions

--- src/NumberSchema.js
+++ src/NumberSchema.js
@@ -4,12 +4,12 @@ export default class Numberschema {
   even() {
     this.validators.push((value) => value % 2 === 0);
     return this;
-  };
-  
+  }
+
   odd() {
     this.validators.push((value) => value % 2 !== 0);
     return this;
-  };
+  }
 
   isValid(value) {
     return this.validators.every((validator) => validator(value) === true);
--- src/Validator.js
+++ src/Validator.js
@@ -1,16 +1,19 @@
-import Numberschema from "./NumberSchema.js";
-import ArraySchema from "./ArraySchema.js";
-import ObjectSchema from "./ObjectSchema.js";
+import Numberschema from './NumberSchema.js';
+import ArraySchema from './ArraySchema.js';
+import ObjectSchema from './ObjectSchema.js';
 
 export default class Validator {
+  // eslint-disable-next-line class-methods-use-this
   number() {
     return new Numberschema();
   }
-  
+
+  // eslint-disable-next-line class-methods-use-this
   array() {
     return new ArraySchema();
   }
-  
+
+  // eslint-disable-next-line class-methods-use-this
   object() {
     return new ObjectSchema();
   }
